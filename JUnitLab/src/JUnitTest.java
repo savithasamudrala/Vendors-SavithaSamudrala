7bcd9aa6ac01038f1b4138e7b5ee7452b719307b
savithasamudrala <savithacsamudrala@gmail.com>
2024-11-21 10:46:37 -0600

    Set up JUnit test cases to validate adding money to a vendor

---
+++ JUnitLab/src/JUnitTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions.*;
+
+public class JUnitTest {
+
+    static Vending vendor;
+    static Item item;
+
+    @BeforeEach
+    public void setup() {
+        vendor = new Vending(10,5);
+    }
+
+    @Test
+    public void addMoneyTest(){
+        vendor.addMoney(1.25);
+        Assertions.assertEquals(1.25, vendor.getBalance());
+    }
+
+    @Test
+    public void addMoneyNegative(){
+        vendor.addMoney(-1.25);
+        Assertions.assertEquals(0, vendor.getBalance());
+    }
+
+    @Test
+    public void addMoneyMax(){
+        vendor.addMoney(1.25);
+        vendor.addMoney(Integer.MAX_VALUE);
+        Assertions.assertEquals(Integer.MAX_VALUE, vendor.getBalance());
+    }
+
+    @Test
+    public void addMoneyMin(){
+        vendor.addMoney(1.25);
+        vendor.addMoney(Integer.MIN_VALUE);
+        Assertions.assertEquals(1.25, vendor.getBalance());
+    }
+
+}
