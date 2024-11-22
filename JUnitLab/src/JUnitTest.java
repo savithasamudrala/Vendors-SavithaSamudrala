ba6311da79d83a2ab7a3c5a258ac3bc7b9497ecd
savithasamudrala <savithacsamudrala@gmail.com>
2024-11-21 21:53:35 -0600

    Fleshed out JUnit test cases

--- JUnitLab/src/JUnitTest.java
+++ JUnitLab/src/JUnitTest.java
@@ -40,6 +40,11 @@ public class JUnitTest {
         Assertions.assertEquals(1.25, vendor.getBalance());
     }
 
+    @Test
+    public void addMoneyZero() {
+        vendor.addMoney(0);
+        Assertions.assertEquals(0, vendor.getBalance());
+    }
 
     @Test
     public void buyItem(){
@@ -62,18 +67,61 @@ public class JUnitTest {
         Assertions.assertEquals(1.25, vendor.getBalance());
     }
 
+    @Test
+    public void buyItemOutOfStock() {
+        for (int i = 0; i < 10; i++) {
+            vendor.addMoney(1.25);
+            vendor.select("Candy");
+        }
+        vendor.select("Candy");
+        Assertions.assertEquals(0, getStockQuantity("Candy"));
+    }
 
     @Test
-    public void emptyStock(){
-        vendor.addMoney(3);
-        vendor.select("Gum");
-        vendor.select("Gum");
-        vendor.select("Gum");
-        vendor.select("Gum");
-        vendor.select("Gum");
-        vendor.select("Gum");
-        Assertions.assertEquals(0.5, vendor.getBalance());
+    public void buyItemStockReduction() {
+        vendor.addMoney(1.25);
+        vendor.select("Candy");
+        Assertions.assertEquals(9, getStockQuantity("Candy"));
     }
+
+    @Test
+    public void refillBalanceToBuy() {
+        vendor.addMoney(0.50);
+        vendor.select("Candy");
+        Assertions.assertEquals(0.50, vendor.getBalance());
+        vendor.addMoney(1.00);
+        vendor.select("Candy");
+        Assertions.assertEquals(0.25, vendor.getBalance());
+    }
+
+
+    @Test
+    public void emptyPartialStock() {
+        vendor.addMoney(2.50);
+        vendor.select("Gum");
+        vendor.select("Gum");
+        Assertions.assertEquals(3, getStockQuantity("Gum"));
+    }
+
+    @Test
+    public void emptyMultipleItems() {
+        for (int i = 0; i < 10; i++) {
+            vendor.addMoney(1.25);
+            vendor.select("Candy");
+        }
+        for (int i = 0; i < 5; i++) {
+            vendor.addMoney(0.50);
+            vendor.select("Gum");
+        }
+        Assertions.assertEquals(0, getStockQuantity("Candy"));
+        Assertions.assertEquals(0, getStockQuantity("Gum"));
+    }
+
+
+    private int getStockQuantity(String name) {
+        return Vending.Stock.get(name).stock;
+    }
+
 
 
 }
